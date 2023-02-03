924efe136ceb41d2cd9d1dbd0452baece9ad169d
alexf37 <xrk4np@virginia.edu>
2023-02-03 02:06:29 -0500

    rework ui

--- public/scripts.js
+++ public/scripts.js
@@ -1,20 +1,56 @@
+//choosing room
+const roomcode = document.getElementById("roomcode");
+const joinform = document.getElementById("joinform");
+const overlay = document.getElementById("overlay");
+
+//content
+const chats = document.getElementById("chats");
+const history = document.getElementById("scripthistory");
+
+//inputs
+const msgform = document.getElementById("msgform");
+const input = document.getElementById("message");
+const checkbox = document.getElementById("toggle");
+
 var socket = io();
 
-var messages = document.getElementById('messages');
-var form = document.getElementById('form');
-var input = document.getElementById('input');
+//join form overlay event handler
+joinform.addEventListener('submit', function (e) {
+    e.preventDefault();
+    if (roomcode.value) {
+        socket.emit("join", roomcode.value);
+    }
+    overlay.style.opacity = "0";
+    overlay.style.pointerEvents = "none";
+});
 
-form.addEventListener('submit', function (e) {
+msgform.addEventListener('submit', function (e) {
     e.preventDefault();
     if (input.value) {
-        socket.emit('chat message', input.value);
+        if(checkbox.checked) {
+            console.log(checkbox.value);
+            socket.emit("script", input.value);
+            let item = document.createElement('li');
+            item.innerHTML = input.value;
+            history.appendChild(item);
+        } else {
+            socket.emit("chat", input.value);
+        }
         input.value = '';
     }
 });
 
-socket.on('chat message', function (msg) {
-    var item = document.createElement('script');
+socket.on('chat', function (msg) {
+    let item = document.createElement('li');
     item.innerHTML = msg;
-    messages.appendChild(item);
+    chats.appendChild(item);
     window.scrollTo(0, document.body.scrollHeight);
 });
+
+socket.on('script', function (msg) {
+    let item = document.createElement('script');
+    item.innerHTML = msg;
+    body.appendChild(item);
+});
+
+addEventListener("submit", (e)=>e.preventDefault());

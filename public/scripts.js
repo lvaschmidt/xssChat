f3eca7c880ef86b63a8f3a0bad5a5c9ec04ad5df
lvaschmidt <lvaschmidt@me.com>
2023-02-05 17:49:03 -0500

    fix missing quotes around "script"

--- public/scripts.js
+++ public/scripts.js
@@ -24,11 +24,11 @@ var username = "Anonymous";
 //join form overlay event handler
 joinform.addEventListener("submit", function (e) {
     e.preventDefault();
-    if(namefield.value) {
+    if (namefield.value) {
         username = namefield.value;
     }
     if (roomcode.value) {
-        socket.emit("join", {room: roomcode.value, name: username});
+        socket.emit("join", { room: roomcode.value, name: username });
         overlay.style.opacity = "0";
         overlay.style.pointerEvents = "none";
 
@@ -42,14 +42,14 @@ joinform.addEventListener("submit", function (e) {
 msgform.addEventListener("submit", function (e) {
     e.preventDefault();
     if (input.value) {
-        if(checkbox.checked) {
+        if (checkbox.checked) {
             console.log(checkbox.value);
-            socket.emit("message", {type: "script", name: username, value: input.value});
+            socket.emit("message", { type: "script", name: username, value: input.value });
             let item = document.createElement('li');
             item.textContent = input.value;
             history.appendChild(item);
         } else {
-            socket.emit("message", {type: "chat", name: username, value: input.value});
+            socket.emit("message", { type: "chat", name: username, value: input.value });
         }
         input.value = '';
     }
@@ -57,32 +57,32 @@ msgform.addEventListener("submit", function (e) {
 
 //tab for element completion
 msgform.addEventListener("keydown", function (e) {
-    if(e.code=="Tab") {
+    if (e.code == "Tab") {
         e.preventDefault();
         let message = input.value;
         let cursorPosition = input.selectionStart;
-        let elemName = message.substring(0,cursorPosition).split(" ").pop().split(">").pop();
+        let elemName = message.substring(0, cursorPosition).split(" ").pop().split(">").pop();
         let insertIndex = cursorPosition - elemName.length;
         try {
-            if(document.createElement(elemName.toLowerCase()).toString() == "[object HTMLUnknownElement]") {
+            if (document.createElement(elemName.toLowerCase()).toString() == "[object HTMLUnknownElement]") {
                 return;
             }
-            
-        } catch(e){
+
+        } catch (e) {
             console.log(e);
             input.focus();
             return;
         }
         let elem = `<${elemName}></${elemName}>`;
-        let newInput = message.slice(0, insertIndex)+elem+message.slice(cursorPosition);
+        let newInput = message.slice(0, insertIndex) + elem + message.slice(cursorPosition);
         input.value = newInput;
-        cursorPosition = (message.slice(0, insertIndex)+elem).length - `</${elemName}>`.length;
+        cursorPosition = (message.slice(0, insertIndex) + elem).length - `</${elemName}>`.length;
         input.setSelectionRange(cursorPosition, cursorPosition);
     }
 });
 
 socket.on('message', function (msg) {
-    if(msg.type==script) {
+    if (msg.type == "script") {
         let item = document.createElement('script');
         item.innerHTML = msg.value;
         scripts.appendChild(item);

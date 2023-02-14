965beb849a95dd2e5fef7d8ff00f34ba449cf6fc
Jacob Singer <jacobsinger@virginia.edu>
2023-02-14 09:32:30 -0500

    Use http2 instead of boring old http

--- index.js
+++ index.js
@@ -2,31 +2,32 @@ var cors = require('cors');
 const express = require("express");
 const compression = require('compression'); //depends on: compression
 const app = express();
-app.use(compression());
-
-const https = require("https");
 const fs = require("fs");
-
 const privateKey = fs.readFileSync('/etc/letsencrypt/live/xsschat.com/privkey.pem');
 const certificate = fs.readFileSync('/etc/letsencrypt/live/xsschat.com/fullchain.pem');
-const credentials = { key: privateKey, cert: certificate };
 
-const httpsServer = https.Server(credentials, app);
+app.use(compression());
+
+//http2 over TLS server
+const httpsServer = require("http2").createSecureServer({
+    allowHTTP1: true,
+    key: privateKey,
+    cert: certificate
+  });
+
 const io = require('socket.io')(httpsServer);
 const port = process.env.PORT || 443;
 
 // redirect any page from http to https
-const http = require('http')
-
+const http = require('http');
 const httpServer = http.createServer((req, res, next) => {
     if (process.env.NODE_ENV !== 'development' && process.env.NODE_ENV !== 'test') {
         res.writeHead(301, { Location: `https://${req.headers.host}${req.url}` });
         res.end()
     } else {
         next();
-    }
+    } 
 }).listen(80);
-
 app.options('*', cors());
 app.use(express.static('public'))
 
